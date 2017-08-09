b06e7270075cbc9bc9ac06d2102b3e38da92ee31
chakrabortyarka007 <chakchak007@gmail.com>
2017-08-10 00:06:00 +0530

    First Commit
    First commit to the messenger spring boot App

---
+++ FNBF-App/FNBF-App/src/main/java/com/org/messenger/FNBFApp/FnbfAppApplication.java
@@ -0,0 +1,12 @@
+package com.org.messenger.FNBFApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnbfAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnbfAppApplication.class, args);
+	}
+}
