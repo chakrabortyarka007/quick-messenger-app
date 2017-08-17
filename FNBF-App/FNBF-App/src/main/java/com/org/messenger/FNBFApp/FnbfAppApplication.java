658545cfe5df5c30be20169a0987a8b69956b5d6
ARKA\Arka <chakchak007@gmail.com>
2017-08-17 18:04:11 +0530

    Properties file updated and config client tested ok

--- FNBF-App/FNBF-App/src/main/java/com/org/messenger/FNBFApp/FnbfAppApplication.java
+++ FNBF-App/FNBF-App/src/main/java/com/org/messenger/FNBFApp/FnbfAppApplication.java
@@ -1,12 +1,25 @@
 package com.org.messenger.FNBFApp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FnbfAppApplication {
+	
+	@Value("${rate}")
+	private String rateFromConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FnbfAppApplication.class, args);
+	}
+	@RequestMapping(value="/rate",method=RequestMethod.GET)
+	public String getConfig() {
+		System.out.println("Rate from config  " +this.rateFromConfig);
+		return this.rateFromConfig;
 	}
 }
