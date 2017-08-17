83e3f80099ec31205365390241346a609aa55398
chakrabortyarka007 <chakchak007@gmail.com>
2017-08-17 23:30:50 +0530

    bootstrap.properties added and property refresh configuration added

--- FNBF-App/FNBF-App/src/main/java/com/org/messenger/FNBFApp/FnbfAppApplication.java
+++ FNBF-App/FNBF-App/src/main/java/com/org/messenger/FNBFApp/FnbfAppApplication.java
@@ -1,25 +1,15 @@
 package com.org.messenger.FNBFApp;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-@RestController
+@ComponentScan(basePackages={"com.org.messenger.*"})
 public class FnbfAppApplication {
 	
-	@Value("${rate}")
-	private String rateFromConfig;
-
 	public static void main(String[] args) {
 		SpringApplication.run(FnbfAppApplication.class, args);
-	}
-	@RequestMapping(value="/rate",method=RequestMethod.GET)
-	public String getConfig() {
-		System.out.println("Rate from config  " +this.rateFromConfig);
-		return this.rateFromConfig;
 	}
 }
