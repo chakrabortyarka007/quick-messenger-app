67400a3b3cfe1079fb4e9c96c43f15e05c042275
ARKA\Arka <chakchak007@gmail.com>
2017-08-25 10:39:49 +0530

    karma added

---
+++ FNBF-Client/src/unit-tests.ts
@@ -0,0 +1,10 @@
+describe('First unit test case',function(){
+    it('Should run default test case',function(){
+        console.log("Arka");
+        expect(true).toBeTruthy;
+    })
+    it('Should run default test cases',function(){
+        console.log("Arka1");
+        expect(true).toBeTruthy;
+    })
+})
