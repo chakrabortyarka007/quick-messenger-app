ea756732ad5d8df8cde6f7e9e80b638e8da24145
chakrabortyarka007 <chakchak007@gmail.com>
2018-02-10 23:00:57 +0530

    # WARNING: head commit changed in the meantime
    Project updated

--- FNBF-App/FNBF-App/src/main/java/com/org/messenger/FNBFApp/FnbfAppApplication.java
+++ FNBF-App/FNBF-App/src/main/java/com/org/messenger/FNBFApp/FnbfAppApplication.java
@@ -2,12 +2,22 @@ package com.org.messenger.FNBFApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import com.org.messenger.service.ISignUpService;
+import com.org.messenger.service.SignUpService;
 
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.org.messenger.*"})
+@EnableNeo4jRepositories
 public class FnbfAppApplication {
+	
+	@Bean
+	public ISignUpService getSignUp(){
+		return new SignUpService();
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FnbfAppApplication.class, args);
